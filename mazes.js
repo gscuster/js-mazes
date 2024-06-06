d2e7c4e2523929da5183fa58fd71f9b583f7ca4e
gscuster <gregory.custer@gmail.com>
2024-06-05 21:56:30 -0400

    Added basic (but broken) generation

--- mazes.js
+++ mazes.js
@@ -7,6 +7,7 @@ let state = {
     [1, 3, 1, 3, 2]
   ],
   mazeSettings: {
+    algorithm: null,
     cellDimensions: [32, 32],
     position: [64, 64],
     lineWidth: 3,
@@ -59,6 +60,11 @@ const drawMaze = (ctx, mazeSettings, maze) => {
   }
 }
 
+const generateMaze = () => {
+  const maze = state.mazeSettings.algorithm(4, 4)
+  state.maze = maze
+}
+
 const preDraw = (canvas, ctx) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
@@ -101,6 +107,30 @@ const setup = () => {
   ctx.font = "30px Verdana";
   window.addEventListener("resize", () => { resizeCanvas(canvas) });
 
+  setupMenu()
+
   resizeMaze(canvas)
+  state.mazeSettings.algorithm = binaryTree
   run(canvas, ctx)
+}
+
+const setupMenu = () => {
+  document.getElementById('buttongenerate').onclick = generateMaze
+}
+
+// Algorithms
+
+const binaryTree = (nCellsX, nCellsY) => {
+  let maze = []
+  // Walls inside maze
+  for (i = 0; i < nCellsY; i++) {
+    maze[i] = []
+    for (j = 0; j < nCellsX; j++) {
+      maze[i][j] = Math.ceil(Math.random()*2)
+    }
+    maze[i][nCellsY] = 2 // Right edge
+  }
+  maze[nCellsY] = Array(nCellsX).fill(1)
+  // Outer edge of maze
+  return maze.reverse()
 }
